1d529e89f6acf392e84c39282757997536bfcb6b
KYPHER26 <167626203+KYPHER26@users.noreply.github.com>
2024-12-23 09:16:52 +0300

    Update General.js

--- Andbad_cmds/General.js
+++ Andbad_cmds/General.js
@@ -55,13 +55,13 @@ zokou({ nomCom: "dev", categorie: "General", reaction: "🎣" }, async (dest, zk
     const { ms, mybotpic } = commandeOptions;
 
     const devs = [
-      { nom: "𝚳𝚪 𝚫𝚴𝐃𝚩𝚫𝐃", numero: "25576818788" },
+      { nom: "✞𓊈𒆜 _𝐊𝐘𝚸𝚮𝚵𝚪_𒆜𓊉 ✞", numero: "25576818788" },
       { nom: "᚛𝚳𝚪 𝚫𝚴𝐃𝚩𝚫𝐃᚜", numero: "255783394967" },
       { nom: "owner Lazack md", numero: "255734980103" },
       // Ajoute d'autres développeurs ici avec leur nom et numéro
     ];
 
-    let message = "WELCOME TO 𝚫𝚴𝐃𝚩𝚫𝐃 MEDIA HELP CENTER! ASK FOR HELP FROM ANY OF THE DEVELOPERS BELOW:\n\n";
+    let message = "WELCOME TO 𒆜 _𝐊𝐘𝚸𝚮𝚵𝚪_𒆜 MEDIA HELP CENTER! ASK FOR HELP FROM ANY OF THE DEVELOPERS BELOW:\n\n";
     for (const dev of devs) {
       message += `----------------\n• ${dev.nom} : https://wa.me/${dev.numero}\n`;
     }
@@ -95,7 +95,7 @@ else {
 zokou({ nomCom: "support", categorie: "General" }, async (dest, zk, commandeOptions) => {
   const { ms, repondre, auteurMessage, } = commandeOptions; 
  
-  repondre("THANK YOU FOR CHOOSING 𝚫𝚴𝐃𝚩𝚫𝐃 MD, HERE ARE OUR SUPPORTIVE CHANNEL\n\n ☉ CHANNEL LINK IS HERE ☉ \n\n❒⁠⁠⁠⁠[https://whatsapp.com/channel/0029VajQn6YF1YlPE0XgBC2m]") 
-  await zk.sendMessage(auteurMessage,{text : `THANK YOU FOR CHOOSING 𝚫𝚴𝐃𝚩𝚫𝐃 MD,MAKE SURE YOU FOLLOW THESE CHANNELS.\n\n https://whatsapp.com/channel/0029VajQn6YF1YlPE0XgBC2m `},{quoted :ms})
+  repondre("THANK YOU FOR CHOOSING KYPHER_XMD, HERE ARE OUR SUPPORTIVE CHANNEL\n\n ☉ CHANNEL LINK IS HERE ☉ \n\n❒⁠⁠⁠⁠[https://whatsapp.com/channel/0029VanspvdLtOj55DG0t82Y]") 
+  await zk.sendMessage(auteurMessage,{text : `THANK YOU FOR CHOOSING KYPHER_XMD,MAKE SURE YOU FOLLOW THESE CHANNELS.\n\n https://whatsapp.com/channel/0029VanspvdLtOj55DG0t82Y `},{quoted :ms})
 
 })
