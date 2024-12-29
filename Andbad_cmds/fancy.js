ce5e6c982e6c0fb6b10f22dbca4c9730e7f69a53
KYPHER26 <167626203+KYPHER26@users.noreply.github.com>
2024-12-29 08:42:48 +0300

    Update fancy.js

--- Andbad_cmds/fancy.js
+++ Andbad_cmds/fancy.js
@@ -7,18 +7,22 @@ zokou({ nomCom: "fancy", categorie: "Fun", reaction: "〽️" }, async (dest, zk
     const text = arg.slice(1).join(" ");
 
     try {
-        if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 Bmw-Md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('KYPHER_XMD', fancy));
+        if (!id || !text.length) {
+            return await repondre(
+                `\nExemple : ${prefixe}fancy 10 Bmw-Md\n` +
+                String.fromCharCode(8206).repeat(4001) +
+                fancy.list('KYPHER_XMD', fancy)
+            );
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
         if (selectedStyle) {
             return await repondre(fancy.apply(selectedStyle, text));
         } else {
-            return await repondre('_Style introuvable :(_');
+            return await repondre('_Style introuvable. Utilisez une ID entre 1 et ' + fancy.length + '._');
         }
     } catch (error) {
         console.error(error);
-        return await repondre('_Une erreur s\'est produite :(_');
+        return await repondre(`_Erreur :_ ${error.message}`);
     }
 });
