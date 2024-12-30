9cb7947aa1a01240bb656925d642faa8d72a7a55
KYPHER26 <167626203+KYPHER26@users.noreply.github.com>
2024-12-30 10:25:49 +0300

    Add files via upload

---
+++ Kypher_cmds/play.js
@@ -0,0 +1,8 @@
+const = {`KYPHER_nizzle`}
+const = Remote_from_ibrahim_adams
+
+{Blocked}={enter password}
+
+//play premium api in the link below
+
+(`https://api.davidcyriltech.my.id/download/ytmp3?url=`)
